52aaa5b1cd26d5e34970aa5dcd0361b69de467e4
Regg44 <kendall.av04@gmail.com>
2024-12-02 15:26:07 -0600

    added asteroids and asteroidfield, helped by boots

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
     dt = 0
     while True:
 
